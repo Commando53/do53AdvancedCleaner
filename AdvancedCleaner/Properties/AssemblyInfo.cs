567f92dd2d5afed1738215ca3fbaf30ba786eed5
Commando53 <105989119+Commando53@users.noreply.github.com>
2022-05-24 19:23:30 +0300

    Create AssemblyInfo.cs

---
+++ AdvancedCleaner/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("AdvancedCleaner")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AdvancedCleaner")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0b6229fc-1ddd-48f2-80ce-b642abf51e64")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
