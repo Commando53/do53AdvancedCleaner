82e3ba5513d56afc7b22506812e6a0132417321c
Commando53 <105989119+Commando53@users.noreply.github.com>
2023-12-05 04:29:33 +0300

    Update AssemblyInfo.cs

--- AdvancedCleaner/Properties/AssemblyInfo.cs
+++ AdvancedCleaner/Properties/AssemblyInfo.cs
@@ -1,18 +1,16 @@
-using System;
-using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Runtime.Versioning;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyTitle("AdvancedCleaner")]
+[assembly: AssemblyTitle("do53AdvancedCleaner")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AdvancedCleaner")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCompany("Commando53's plugins")]
+[assembly: AssemblyProduct("do53AdvancedCleaner")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("0b6229fc-1ddd-48f2-80ce-b642abf51e64")]
+[assembly: Guid("ab6e01f6-cba1-49df-8b5f-a65715a875b9")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
