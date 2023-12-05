2d3b301a595f6062a23ade7ba81838e7d317114e
Commando53 <105989119+Commando53@users.noreply.github.com>
2023-12-05 04:27:03 +0300

    Update AdvancedCleanerConfiguration.cs

--- AdvancedCleaner/AdvancedCleanerConfiguration.cs
+++ AdvancedCleaner/AdvancedCleanerConfiguration.cs
@@ -1,15 +1,17 @@
+using Rocket.API;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Rocket.API;
 
-namespace AdvancedCleaner
+namespace do53AdvancedCleaner
 {
-    public class Configuration : IRocketPluginConfiguration, IDefaultable
+    public class do53AdvancedCleanerConfiguration : IRocketPluginConfiguration, IDefaultable
     {
         public void LoadDefaults() { }
         public float DefaultRadius = 30;
+        public bool AskForConfirmBeforeCleaning = true;
     }
+
 }
